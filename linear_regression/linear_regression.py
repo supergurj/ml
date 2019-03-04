f0ea337982916eb53301a31ae6387a70d978b526
supergurj <supergurj@gmail.com>
2019-03-04 22:16:57 +0000

    Checkin initial experiments

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+def ScaleAndNormalise( v ):
+
+    mean = np.mean(v, axis=0)
+    sum = np.sum(v, axis=0)
+
+    v = np.subtract(v, mean)
+    v = np.divide(v, sum)
+
+    return (v, mean, sum )
+
+# Load csv file
+df = pd.read_csv("house_data_small.csv")
+
+# Read data into dataframes
+X = df[["sq_feet", "num_bedrooms", "num_bathrooms"]].values
+Y = df[["sale_price"]]
+
+# Scale and normalise input
+# ret = ScaleAndNormalise( X )
+# X = ret[0]
+# X = np.insert( X, 0, 1, axis=1 )
+
+
+print(X)
+print(Y)
+
+# for key, value in df.iteritems():
+#
+#     mean = value.mean()
+#     range = value.max() - value.min()
+#
+#     print ( key, mean, range )
+#
+#     for i in range( dim[1] ):
+#         print ( v[ cur, i ] + ' ')
+
+
+
+
